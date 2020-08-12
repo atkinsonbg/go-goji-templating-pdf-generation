01b6d2b27b1af11df374c26983fe4244a563feda
Brandon Atkinson <tehremo@gmail.com>
2020-08-11 20:19:21 -0400

    Fixed health route

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/atkinsonbg/go-goji-templating-pdf-generation/handlers"
 	"goji.io"
+	"goji.io/pat"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := goji.NewMux()
-	mux.HandleFunc(pat.Get("/health"), handlers.health)
-
+	mux.HandleFunc(pat.Get("/health"), handlers.HealthHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
